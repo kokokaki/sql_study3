1474da8ad54ae58453854d6293c46a2c0df03208
kokokaki <hsg9984@gmail.com>
2021-08-30 11:52:56 +0900

    [21.08.30] # 계층형 쿼리, 서브쿼리

--- 예제/8. 집합연산자, 계층형쿼리 예제.sql
+++ 예제/8. 집합연산자, 계층형쿼리 예제.sql
@@ -110,6 +110,9 @@ ON A.dept_cd = B.dept_cd
 START WITH A.direct_manager_emp_no IS NULL
 CONNECT BY PRIOR A.emp_no = A.direct_manager_emp_no;
 
+
+
+
 SELECT 
     LEVEL AS LVL,
     LPAD(' ', 4*(LEVEL-1)) || emp_no || '(' || emp_nm || ')' AS "조직인원",
@@ -122,3 +125,9 @@ JOIN tb_dept B
 ON A.dept_cd = B.dept_cd
 START WITH A.direct_manager_emp_no = '1000000037'
 CONNECT BY A.emp_no = PRIOR A.direct_manager_emp_no;
+
+
+
+
+
+
